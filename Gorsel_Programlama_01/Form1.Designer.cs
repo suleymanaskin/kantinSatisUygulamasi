429f4670099c5519e2148144136e310051c10777
Süleyman Aşkın <suleymanaskin35@gmail.com>
2026-09-23 00:39:08 +0300

    Proje dosyası ekle.

---
+++ Gorsel_Programlama_01/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Gorsel_Programlama_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.cbx_urunler = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_kapat = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(93, 225);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(75, 23);
+            this.btn_ekle.TabIndex = 0;
+            this.btn_ekle.Text = "Ekle";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // cbx_urunler
+            // 
+            this.cbx_urunler.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_urunler.FormattingEnabled = true;
+            this.cbx_urunler.Location = new System.Drawing.Point(93, 120);
+            this.cbx_urunler.Name = "cbx_urunler";
+            this.cbx_urunler.Size = new System.Drawing.Size(121, 21);
+            this.cbx_urunler.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ürünler";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(250, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(696, 319);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Adet";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(323, 417);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 420);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Toplam Tutar";
+            // 
+            // btn_kapat
+            // 
+            this.btn_kapat.Location = new System.Drawing.Point(495, 415);
+            this.btn_kapat.Name = "btn_kapat";
+            this.btn_kapat.Size = new System.Drawing.Size(177, 23);
+            this.btn_kapat.TabIndex = 10;
+            this.btn_kapat.Text = "Satış İşlemini Kapat";
+            this.btn_kapat.UseVisualStyleBackColor = true;
+            this.btn_kapat.Click += new System.EventHandler(this.btn_kapat_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Alınan Ürünler";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(93, 177);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 509);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_kapat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbx_urunler);
+            this.Controls.Add(this.btn_ekle);
+            this.Name = "Form1";
+            this.Text = "Kantin Satış Otomasyonu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.ComboBox cbx_urunler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_kapat;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Gorsel_Programlama_01/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gorsel_Programlama_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Urun> urunListesi = new List<Urun>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("urunler.txt", Encoding.UTF8);
+            string oku = " ";
+            while ((oku = sr.ReadLine()) != null)
+            {
+                string[] parca = oku.Split('|');
+                Urun u = new Urun();
+                u.urun_id = Convert.ToInt32(parca[0]);
+                u.urun_adi = parca[1];
+                u.urun_fiyati = Convert.ToDecimal(parca[2]);
+                urunListesi.Add(u);
+
+
+            }
+            sr.Close();
+            cbx_urunler.DisplayMember = "urunAd_Fiyat";
+            cbx_urunler.ValueMember = "urun_id";
+            cbx_urunler.DataSource = urunListesi;
+        }
+        List<UrunListe> satilanUrunler = new List<UrunListe>();
+        public class Urun {
+            public int urun_id { get; set; }
+            public string urun_adi { get; set; }
+            public decimal urun_fiyati { get; set; }
+            public string urunAd_Fiyat { get{ return urun_adi + " - " + urun_fiyati + " TL"; } }
+            public Urun() { 
+            
+            }
+        }
+        public class UrunListe { 
+         public Urun urun { get; set; }
+         public string urunAd { get; set; }
+            public decimal urunFiyat { get; set; }
+            public int adet { get; set; }
+            public decimal Tutar { get { return urunFiyat * adet; } }
+
+        }
+
+        private void btn_ekle_Click(object sender, EventArgs e)
+        {
+            UrunListe ul = new UrunListe();
+            ul.urun = (Urun)cbx_urunler.SelectedItem;
+            ul.urunAd = ul.urun.urun_adi;
+            ul.urunFiyat = ul.urun.urun_fiyati;
+            ul.adet = (int)(numericUpDown1.Value);
+            satilanUrunler.Add(ul);
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = satilanUrunler;
+            dataGridView1.Columns[0].Visible = false;
+
+            decimal toplamtutar = 0;
+            foreach (var item in satilanUrunler)
+            {
+                toplamtutar += item.Tutar;
+            }
+            textBox1.Text = toplamtutar.ToString();
+        }
+
+        private void btn_kapat_Click(object sender, EventArgs e)
+        {
+           
+               StreamWriter sw = new StreamWriter("satislar.txt", true, Encoding.UTF8);
+               String tarih = DateTime.Now.ToString("dd.MM.yyyy.HH:mm:ss");
+            foreach (var item in satilanUrunler)
+                {
+                    sw.WriteLine(tarih + "|" + item.urun.urun_adi + "|" + item.adet + "|" + item.Tutar);
+                }
+                sw.Close();
+                
+                satilanUrunler= null;
+                dataGridView1.DataSource = satilanUrunler;
+                textBox1.Text = " ";
+                cbx_urunler.SelectedIndex = 0;
+                numericUpDown1.Value = 0;
+                MessageBox.Show("Satışlar kaydedildi");
+            
+         
+        }
+    }
+}
